bbb3449cf9d4401ebea618faeb33389d55712c23
codewithmayor <mayor.michael19@gmail.com>
2024-06-27 11:55:27 +0200

    Made some more changes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Welcome to Oluwamayowa Olawumi's website!");
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButton = document.getElementById('toggleButton');
+    const moreInfo = document.getElementById('moreInfo');
+
+    toggleButton.addEventListener('click', function() {
+        if (moreInfo.style.display === 'none') {
+            moreInfo.style.display = 'block';
+            toggleButton.textContent = 'Show Less About Me';
+        } else {
+            moreInfo.style.display = 'none';
+            toggleButton.textContent = 'Show More About Me';
+        }
+    });
+});
